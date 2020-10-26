32a9001decdabdcb1b7136f7b3d1ef820ca416a0
Hansanghyeon <dandan9509@gmail.com>
2020-10-26 23:54:53 +0900

    fix: 스크립트 명령어 오타수정

--- quick_til.sh
+++ quick_til.sh
@@ -1,4 +1,4 @@
 read -p "지정할 날짜(YYYY-MM-DD): " date
 read -p "학습링크(ex. youtube): " youtube
 
-gh create --title "[$date] 학습목표" --body "🔖 Start: [youtube]($youtube)"
+gh issue create --title "[$date] 학습목표" --body "🔖 Start: [youtube]($youtube)"
